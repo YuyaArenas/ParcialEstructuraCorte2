2fe7cc5ba694bcd02f72fd348771e295448a8ea2
yuyiana <yuliana.arenas01@unicatolica.edu.co>
2026-04-13 20:03:40 -0500

    finishpila

--- pila.js
+++ pila.js
@@ -6,18 +6,21 @@ class Pila{
         this.Pila.push(element);
     }
     pop(){
-        return this.pila.pop();
+        return this.Pila.pop();
     }
     isEmpty(){
-        return this.pila.length === 0;
+        return this.Pila.length === 0;
     }
     size(){
-        return this.pila.length;
+        return this.Pila.length;
     }
 }
 
-let pila = new pila();
+let pila = new Pila();
 pila.push({nombre:"Lisa", edad:27});
 pila.push({nombre:"Jennie", edad:30});
 console.log(pila.pop());
-
+console.log(pila.size());
+console.log(pila.isEmpty());
+pila.pop();
+console.log(pila.isEmpty());
